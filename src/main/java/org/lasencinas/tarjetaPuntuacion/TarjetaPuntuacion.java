c6808ab8f589bfa3846a5f64f0393a2756d7d703
alexnro <alexjandron99@gmail.com>
2019-02-01 02:31:48 +0100

    Method computarTarjete() improved

--- src/main/java/org/lasencinas/tarjetaPuntuacion/TarjetaPuntuacion.java
+++ src/main/java/org/lasencinas/tarjetaPuntuacion/TarjetaPuntuacion.java
@@ -53,17 +53,23 @@ public class TarjetaPuntuacion {
 
         for (int bola = 0; bola < puntuacion.length(); bola++) {
             char punto = puntuacion.charAt(bola);
-            if (!isSpare(punto)) {
-                if (puntuacion.indexOf(punto) != -1) {
-                    total += this.pins.indexOf(punto);
-                }
-            } else if (isSpare(punto)) {
+            if ((bola == puntuacion.length()-1) && (puntuacion.charAt(bola-1) == '/')){
+                break;
+            }
+            if (isSpare(punto)) {
                 char spare = puntuacion.charAt(bola + 1);
                 char pinAterior = puntuacion.charAt(bola - 1);
                 total += 10 + this.pins.indexOf(spare) - this.pins.indexOf(pinAterior);
             } else if (isStrike(punto)) {
-                //TODO
-                ;
+                char proximoPrimerPin = puntuacion.charAt(bola + 1);
+                char proximoSegundoPin = puntuacion.charAt(bola + 2);
+                char pinAterior = puntuacion.charAt(bola - 1);
+                total += 10 + this.pins.indexOf(proximoPrimerPin) + this.pins.indexOf(proximoSegundoPin);
+            }
+            else {
+                if (puntuacion.indexOf(punto) != -1) {
+                    total += this.pins.indexOf(punto);
+                }
             }
         }
         return total;
