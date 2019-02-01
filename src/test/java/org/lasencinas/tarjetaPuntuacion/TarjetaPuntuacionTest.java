7432dc307ead2719966cd3d9d012a526f8437dd0
alexnro <alexjandron99@gmail.com>
2019-02-01 20:19:08 +0100

    Deleted some useless tests

--- src/test/java/org/lasencinas/tarjetaPuntuacion/TarjetaPuntuacionTest.java
+++ src/test/java/org/lasencinas/tarjetaPuntuacion/TarjetaPuntuacionTest.java
@@ -15,45 +15,13 @@ public class TarjetaPuntuacionTest {
         tarjeta = new TarjetaPuntuacion();
     }
 
-    @Test
-    public void computarStrikeTest() {
-
-        assertEquals(tarjeta.getStrike(), tarjeta.computarStrike('X'));
-        assertEquals(0, tarjeta.computarStrike('9'));
-    }
-
-    @Test
-    public void computarPinsTest() {
-
-        String pins = "-123456789";
-        assertEquals(0, tarjeta.computarPins('-'));
-
-        for (int i = 0; i < pins.length(); i++) {
-            assertEquals(i, tarjeta.computarPins(pins.charAt(i)));
-        }
-
-        /*Other way to write de test
-        for(Integer i = 1; i <= 9; i++) {
-            assertEquals(i.intValue(), tarjeta.computarPins(i.toString()));
-        }
-        */
-    }
 
     @Test
     public void computarTarjetaVeinteBolasTest() {
 
         String tarjetaVeinteBolas = "11111111111111111111";
-        tarjeta = new TarjetaPuntuacion(tarjetaVeinteBolas);
 
         assertEquals(20, tarjeta.computarTarjeta(tarjetaVeinteBolas));
-    }
-
-    @Test
-    public void computarSpareTest() {
-
-        assertEquals(10, tarjeta.computarSpare('/'));
-        assertEquals(0, tarjeta.computarSpare('2'));
-        assertEquals(0, tarjeta.computarSpare('-'));
     }
 
     @Test
