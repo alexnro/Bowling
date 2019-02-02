ca09d8da604b12bebbc1a2997da7a656f7837421
alexnro <alexjandron99@gmail.com>
2019-02-02 03:28:06 +0100

    Modified one test case

--- src/test/java/org/lasencinas/tarjetaPuntuacion/TarjetaPuntuacionTest.java
+++ src/test/java/org/lasencinas/tarjetaPuntuacion/TarjetaPuntuacionTest.java
@@ -47,7 +47,6 @@ public class TarjetaPuntuacionTest {
         assertEquals(77, tarjeta.computarTarjeta(puntuacion));
     }
 
-    //Test failed
     @Test
     public void computarTarjetaSpareTest2() {
 
